f1df0bdee748083d4b94a33af879e93a9ef4ac63
Neha Sen <neha712513@gmail.com>
2022-02-21 17:01:53 +0530

    done

---
+++ Template-U3C4/components/sidebar.js
@@ -0,0 +1,12 @@
+function sidebar() {
+    return `<div> <a href="index.html">Home</a></div>
+    <div>
+      <input type="text" id="searchbar" placeholder="Search">
+    </div>
+    <div>Startups</div>
+    <div>Newsletters</div>`;
+
+    // return your html component here
+    //Make sure to give input search box id as ""
+}
+export default sidebar;
